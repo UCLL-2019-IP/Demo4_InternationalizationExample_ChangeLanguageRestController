278afacc0ebe07dcb4d04c22b496ddeec7de8370
Elke Steegmans <Elke.Steegmans@ucll.be>
2019-04-24 21:10:29 +0200

    Initial commit

---
+++ src/main/java/be/ucll/ip/internationalization/InternationalizationApp.java
@@ -0,0 +1,12 @@
+package be.ucll.ip.internationalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalizationApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternationalizationApp.class, args);
+    }
+}
